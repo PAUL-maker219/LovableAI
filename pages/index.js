bc2871ba5f8ecf1e8fdea7cb08558d0893c02c51
PAUL-maker219 <paullubasha06@gmail.com>
2025-07-11 20:04:34 +0300

    Delete pages directory

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ textAlign: 'center', marginTop: '4rem' }}>
-      <h1>🚀 Deriv Trading AI App is Deployed!</h1>
-      <p>This homepage confirms your app is live.</p>
-      <p>Use the /api/trade endpoint to trigger live trades with Deriv.</p>
-    </div>
-  );
-}
--- pages/pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ textAlign: 'center', marginTop: '4rem' }}>
-      <h1>🚀 Deriv Trading AI App is Deployed!</h1>
-      <p>This homepage confirms your app is live.</p>
-    </div>
-  );
-}
