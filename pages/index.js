80d6b02df72813fc218c4fc442598248a0242b32
PAUL-maker219 <paullubasha06@gmail.com>
2025-07-11 20:16:31 +0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '4rem' }}>
+      <h1>🚀 Deriv Trading AI App is Deployed!</h1>
+      <p>This homepage confirms your app is live and working on Vercel.</p>
+      <p>Next, you can add live trade features using the Deriv API.</p>
+    </div>
+  );
+}
